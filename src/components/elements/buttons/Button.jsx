caedb11aab0e849ee00ecc349f51970590cd9104
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-01-31 23:05:36 +0100

    chore(ui): update Button component - outlined, neon

--- src/components/elements/buttons/Button.jsx
+++ src/components/elements/buttons/Button.jsx
@@ -15,6 +15,8 @@ const buttonStyles = cva("font-medium text-sm text-center inline-flex items-cent
             primary: "text-white bg-primary-500 dark:bg-primary-300 hover:bg-primary-500/90 dark:hover:bg-primary-300/90",
             mono: "text-white bg-[#24292F] hover:bg-[#24292F]/90 dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30",
             danger: "text-white bg-red-500 dark:bg-red-400 hover:bg-red-400/90 dark:focus:ring-red-200 dark:hover:bg-red-600",
+            neon: "text-blue-800 bg-blue-100 dark:bg-blue-900 dark:text-blue-300 text-blue-400 hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300",
+            outlined: "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800",
         },
         rounded: {
             default: "rounded-lg",
@@ -30,7 +32,7 @@ const buttonStyles = cva("font-medium text-sm text-center inline-flex items-cent
     },
 });
 
-const Button = ({icon = false, label, size, intent, rounded, color, ...props}) => {
+const Button = ({variant, icon = false, label, size, intent, rounded, color, ...props}) => {
     return (
         // <button type="button" className={`${sizeClasses[size]} ${roundedClasses[rounded]} ${colorClasses[color]} ${variantClasses[variant]} font-medium text-sm text-center inline-flex items-center`}>
         <ButtonOrLink className={buttonStyles({intent, size, color, rounded})} {...props}>
