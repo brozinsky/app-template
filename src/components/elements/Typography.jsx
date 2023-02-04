2a682917bc939f7519ac4faec54737e4507c944b
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-02-04 23:49:53 +0100

    chore(ui, typography): add cva, add alignment, add decoration

--- src/components/elements/Typography.jsx
+++ src/components/elements/Typography.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "clsx";
+import {cva} from "class-variance-authority";
 
 const variants = {
     h1: "h1",
@@ -14,22 +15,33 @@ const variants = {
     body2: "p",
 };
 
-const typographySizes = {
-    h1: "text-6xl",
-    h2: "text-5xl",
-    h3: "text-4xl",
-    h4: "text-3xl",
-    h5: "text-2xl",
-    h6: "text-1xl",
-    body1: "text-base",
-};
-
-const Typography = ({variant, size, children, className, ...props}) => {
+const Typography = ({center, variant, decoration, size, children, className = "mb-4", ...props}) => {
     const Component = variant ? variants[variant] : "p";
-    const typographySize = size ? typographySizes[size] : typographySizes["p"];
+
+    const classes = cva([className, "tracking-wide"], {
+        variants: {
+            size: {
+                h1: "text-6xl font-medium",
+                h2: "text-5xl font-medium",
+                h3: "text-4xl font-medium",
+                h4: "text-3xl font-medium",
+                h5: "text-2xl font-medium",
+                h6: "text-1xl font-medium",
+                body1: "text-base",
+            },
+            decoration: {
+                lineTop: "flex flex-col gap-2 before:h-[2px] before:w-[100px] flex before:bg-primary-500 before:rounded-full",
+                lineBot: "flex flex-col gap-2 after:h-[2px] after:w-[100px] flex after:bg-primary-500 after:rounded-full",
+                none: "",
+            },
+            center: {
+                true: "text-center items-center",
+            },
+        },
+    });
 
     return (
-        <Component className={classNames(typographySize, className)} {...props}>
+        <Component className={classes({size, decoration, center})} {...props}>
             {children}
         </Component>
     );
