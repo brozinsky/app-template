d5c5b95d5fe368c3a2c688137c584a959c63a974
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-02-05 21:47:34 +0100

    chore(utils): add variants objects utils

---
+++ src/utils/variants.js
@@ -0,0 +1,19 @@
+export const containerVariants = {
+    div: "div",
+    section: "section",
+    main: "main",
+    nav: "nav",
+    article: "article",
+    footer: "footer",
+    header: "header",
+};
+
+export const typographyVariants = {
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    h4: "h4",
+    h5: "h5",
+    h6: "h6",
+    body1: "p",
+};
