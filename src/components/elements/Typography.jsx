03b52e787e63a719e20d970aa6bcf42b076bd5b4
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-02-05 23:25:00 +0100

    refactor: add variants imports to Typography and Container

--- src/components/elements/Typography.jsx
+++ src/components/elements/Typography.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-import classNames from "clsx";
 import {cva} from "class-variance-authority";
-
-const variants = {
-    h1: "h1",
-    h2: "h2",
-    h3: "h3",
-    h4: "h4",
-    h5: "h5",
-    h6: "h6",
-    subheading1: "h6",
-    subheading2: "h6",
-    body1: "p",
-    body2: "p",
-};
+import {typographyVariants} from "@/utils/variants";
 
 const Typography = ({center, variant, decoration, size, children, className = "mb-4", ...props}) => {
-    const Component = variant ? variants[variant] : "p";
+    const Component = variant ? typographyVariants[variant] : "p";
 
     const classes = cva([className, "tracking-wide"], {
         variants: {
--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import classNames from "clsx";
-
-const variants = {
-    div: "div",
-    section: "section",
-    main: "main",
-    nav: "nav",
-    article: "article",
-    footer: "footer",
-    header: "header",
-};
+import {containerVariants} from "@/utils/variants";
 
 const Container = ({variant, children, className, ...props}) => {
-    const Component = variant ? variants[variant] : "div";
+    const Component = variant ? containerVariants[variant] : "div";
 
     return (
         <Component className={classNames("container mx-auto", className)} {...props}>
