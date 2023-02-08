eb05178e66a293b4410b223fdeba269b2731c930
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-02-08 23:47:43 +0100

    chore(layout, container): add noMarign variant to Container

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import classNames from "clsx";
 import {containerVariants} from "@/utils/variants";
 
-const Container = ({variant, children, className, ...props}) => {
+const Container = ({variant, children, className, noMargin, ...props}) => {
     const Component = variant ? containerVariants[variant] : "div";
 
     return (
-        <Component className={classNames("container mx-auto", className)} {...props}>
+        <Component className={classNames("container", noMargin ? "" : "mx-auto", className)} {...props}>
             {children}
         </Component>
     );
