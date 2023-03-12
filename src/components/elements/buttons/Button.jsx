174568ff5647dfc7651bce30927c465aa9cf4960
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-03-13 00:26:09 +0100

    chore(button): initialize animations, add variants

--- src/components/elements/buttons/Button.jsx
+++ src/components/elements/buttons/Button.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import {cva} from "class-variance-authority";
 import ButtonOrLink from "./ButtonOrLink";
+import {useEffect} from "react";
 
 const buttonStyles = cva("font-medium text-m text-center inline-flex items-center", {
     variants: {
         variant: {
             primary: "text-white bg-primary-500 dark:bg-primary-300 hover:bg-primary-500/90 dark:hover:bg-primary-300/90",
+            shine: "shine text-white bg-primary-500 dark:bg-primary-300 hover:bg-primary-500/90 dark:hover:bg-primary-300/90",
             mono: "text-white bg-[#24292F] hover:bg-[#24292F]/90 dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30",
             danger: "text-white bg-red-500 dark:bg-red-400 hover:bg-red-400/90 dark:focus:ring-red-200 dark:hover:bg-red-600",
             neon: "text-blue-800 bg-blue-100 dark:bg-blue-900 dark:text-blue-300 text-blue-400 hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300",
@@ -19,6 +21,10 @@ const buttonStyles = cva("font-medium text-m text-center inline-flex items-cente
             default: "rounded-lg",
             none: "",
             full: "rounded-full",
+        },
+        animates: {
+            true: "ping",
+            false: "",
         },
     },
     defaultVariants: {
@@ -45,9 +51,22 @@ const labelStyles = cva(null, {
 const Button = ({isUppercase, startIcon, endIcon, variant, label, size, intent, rounded, color, ...props}) => {
     const marginLeft = startIcon !== undefined;
     const marginRight = endIcon !== undefined;
+    const [animates, setAnimate] = useState(false);
+
+    const handleClick = () => {
+        setAnimate(true);
+        setTimeout(() => {
+            setAnimate(false);
+        }, 150);
+    };
+
+    useEffect(() => {
+        console.log(animates);
+    }, [animates]);
+
     return (
         // <button type="button" className={`${sizeClasses[size]} ${roundedClasses[rounded]} ${colorClasses[color]} ${variantClasses[variant]} font-medium text-sm text-center inline-flex items-center`}>
-        <ButtonOrLink className={buttonStyles({variant, intent, size, color, rounded})} {...props}>
+        <ButtonOrLink onClick={handleClick} className={buttonStyles({animates, variant, intent, size, color, rounded})} {...props}>
             {startIcon ? startIcon : null}
             <span className={labelStyles({marginLeft, marginRight, isUppercase})}>{label}</span>
             {endIcon ? endIcon : null}
